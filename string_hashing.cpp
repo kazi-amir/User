97fcdec5e338a654805020136e0457304fa63d65
Kazi Amir Hamza <82359890+kazi-amir@users.noreply.github.com>
2024-10-11 00:11:49 +0600

    Update string_hashing

--- string_hashing.cpp
+++ string_hashing.cpp
@@ -4,13 +4,10 @@ int pow_b1[mxN], pow_b2[mxN], inv_b1[mxN], inv_b2[mxN];
 int binExp(int base, int power, int mod){
     int res = 1;
     while(power){
-        if(power&1){
-            res = (1LL * res * base)%mod;
-        }
+        if(power&1) res = (1LL * res * base)%mod;
         base = (1LL * base * base) % mod;
         power>>=1;
-    }
-    return res;
+    } return res;
 }
 void pre(){
     pow_b1[0] = pow_b2[0] = 1;
@@ -37,8 +34,7 @@ vector<pair<int, int>> getPref(string &s){
             hsh[i].first = (hsh[i-1].first + (1LL * s[i] * pow_b1[i])%mod1)%mod1;
             hsh[i].second = (hsh[i-1].second + (1LL * s[i] * pow_b2[i])%mod2)%mod2;
         }
-    }
-    return hsh;
+    } return hsh;
 }
 pair<int,int> getHash(string &str){
     int hsh1 = 0, hsh2 = 0, sz = str.size();
@@ -47,20 +43,14 @@ pair<int,int> getHash(string &str){
     }
     for(int i = 0; i < sz; ++i){
         hsh2 = (hsh2 + 1LL * str[i] * pow_b2[i] % mod2) % mod2;
-    }
-    return {hsh1, hsh2};
+    } return {hsh1, hsh2};
 }
 pair<int, int> getSub(int l, int r, vector<pair<int, int>> &v){
     pair<int, int> q;
-    if(l==0){
-        q = {v[r].first, v[r].second};
-    }
+    if(l==0){ q = {v[r].first, v[r].second}; }
     else{
-        int x = (v[r].first-v[l-1].first+mod1)%mod1;
-        x = (1LL * x * inv_b1[l])%mod1;
-        int y = (v[r].second-v[l-1].second+mod2)%mod2;
-        y = (1LL * y * inv_b2[l])%mod2;
+        int x = (1LL * ((v[r].first-v[l-1].first+mod1)%mod1) * inv_b1[l])%mod1;
+        int y = (1LL * ((v[r].second-v[l-1].second+mod2)%mod2) * inv_b2[l])%mod2;
         q = {x,y};
-    }
-    return q;
+    } return q;
 }
