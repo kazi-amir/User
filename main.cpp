e2ca4891c8be72905712bbfc513e054d8221c2c3
kazi-amir <kaziah444@gmail.com>
2024-11-09 11:05:08 +0600

    ...

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+void solve(){
+    int n; cin>>n; cout<<n<<endl;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int tcase = 1; 
+    //cin>>tcase;
+    for(int tc = 1; tc<=tcase; tc++){
+        // cout<<"Case #"<<tc<<": ";
+        solve();
+    }
+}
