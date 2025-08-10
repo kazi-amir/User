ada9d0b32a1d2fb4e073fb74c32d54b58b89746c
kazi-amir <kaziah444@gmail.com>
2025-08-10 14:47:27 +0600

    ...

--- CP_Codes/D.cpp
+++ CP_Codes/D.cpp
@@ -1,22 +1,127 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-int main(){
-    int x, y, a, b, c;
-    scanf("%d %d %d %d %d", &x, &y, &a, &b, &c);
-    if(x<=0 || x>1000000 || y<=0 || y>x || y>1000000){
-        printf("Error\n");
+char noun[100][100] = {"Moushi", "Hamza", "Mim", "Cat", "Boltu", "pizza"};
+char pronoun[100][100] = {"I", "you", "he", "she", "me"};
+char verb[100][100] = {"do", "work", "eat", "run", "swim"};
+char adjective[100][100] = {"big", "small", "happy", "sad", "red"};
+char adverb[100][100] = {"quickly", "very", "yesterday", "here", "extremely"};
+char preposition[100][100] = {"in", "on", "at", "with", "from"};
+char conjunction[100][100] = {"and", "but", "or", "so", "because"};
+char interjection[100][100] = {"Wow", "Ouch", "Hey", "Oh", "Oops"};
+
+
+int main() {
+    char str[100];
+    fgets(str, 100, stdin);
+
+    int lenNoun = sizeof(noun) / sizeof(noun[0]);
+    int lenPron = sizeof(pronoun) / sizeof(pronoun[0]);
+    int lenVerb = sizeof(verb) / sizeof(verb[0]);
+    int lenAdj = sizeof(adjective) / sizeof(adjective[0]);
+    int lenAdv = sizeof(adverb) / sizeof(adverb[0]);
+    int lenPrep = sizeof(preposition) / sizeof(preposition[0]);
+    int lenConj = sizeof(conjunction) / sizeof(conjunction[0]);
+    int lenInte = sizeof(interjection) / sizeof(interjection[0]);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
+            //Meaw
+        }
+        else {
+            str[i] = ' ';
+        }
     }
-    if(a<0 || b<0 || c<0 || a>100000 || b>100000 || c>100000){   
-        printf("Error\n");
+
+    char *p = strtok(str, " ");
+    while (p != NULL) {
+        printf("%s: ", p);
+        int isFound = 0; //if any parts of speeh is similar
+        // is a noun?
+        for (int i = 0; i < lenNoun; i++) {
+            if (strcmp(noun[i], p) == 0) {
+                printf("Noun\n");
+                isFound = 1;
+                break;
+            }
+        }
+        if (isFound == 0) {
+            for (int i = 0; i < lenPron; i++) {
+                if (strcmp(pronoun[i], p) == 0) {
+                    printf("Pronoun\n");
+                    isFound = 1;
+                    break;
+                }
+            }
+        }
+        if (isFound == 0) {
+            for (int i = 0; i < lenVerb; i++) {
+                if (strcmp(verb[i], p) == 0) {
+                    printf("Verb\n");
+                    isFound = 1;
+                    break;
+                }
+            }
+        }
+        if (isFound == 0) {
+            for (int i = 0; i < lenVerb; i++) {
+                if (strcmp(verb[i], p) == 0) {
+                    printf("Verb\n");
+                    isFound = 1;
+                    break;
+                }
+            }
+        }
+        if (isFound == 0) {
+            for (int i = 0; i < lenAdj; i++) {
+                if (strcmp(adjective[i], p) == 0) {
+                    printf("Adjective\n");
+                    isFound = 1;
+                    break;
+                }
+            }
+        }
+        if (isFound == 0) {
+            for (int i = 0; i < lenAdv; i++) {
+                if (strcmp(adverb[i], p) == 0) {
+                    printf("Adverb\n");
+                    isFound = 1;
+                    break;
+                }
+            }
+        }
+        if (isFound == 0) {
+            for (int i = 0; i < lenPrep; i++) {
+                if (strcmp(preposition[i], p) == 0) {
+                    printf("Preposition\n");
+                    isFound = 1;
+                    break;
+                }
+            }
+        }
+        if (isFound == 0) {
+            for (int i = 0; i < lenConj; i++) {
+                if (strcmp(conjunction[i], p) == 0) {
+                    printf("Conjunction\n");
+                    isFound = 1;
+                    break;
+                }
+            }
+        }
+        if (isFound == 0) {
+            for (int i = 0; i < lenInte; i++) {
+                if (strcmp(interjection[i], p) == 0) {
+                    printf("Interjection\n");
+                    isFound = 1;
+                    break;
+                }
+            }
+        }
+        if (isFound == 0) {
+            printf("Unknown\n");
+        }
+        p = strtok(NULL, " ");
     }
-    int res = a+b+c;
-    if(res>=x){
-        printf("Chill guy\n");
-    }
-    else if(res>=y){
-        printf("Who knows\n");
-    }
-    else{
-        printf("A bad day\n");
-    }
+
 }
--- CP_Codes/main.cpp
+++ CP_Codes/main.cpp
@@ -1,56 +1,60 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-#define ll long long
-#define endl '\n'
+int main() {
+  char str[100];
+  fgets(str, 100, stdin);
 
-/*
-Definition: dp[n] =
-Output:
-Transitions:
-    max for all cases.
-*/
+  char tokens[100][100];
 
-void solve() {
-    int t, a, b; cin >> t >> a >> b;
-    int pos[t + 10] {};
-    pos[0] = 1;
-    for (int i = 0; i <= t; i++) {
-        if (pos[i]) {
-            if (i + a <= t) pos[i + a] = 1;
-            if (i + b <= t) pos[i + b] = 1;
+  int ind = 0; // koyta token ache tar count
+  char *p = strtok(str, " ");
+  while (p != NULL) {
+    //storing each string in the token table
+    int j = 0;
+    for (j = 0; p[j + 1] != '\0'; j++) {
+      tokens[ind][j] = p[j];
+      tokens[ind][j + 1] = '\0';
+    }
+    if ((p[j] >= 'a' && p[j] <= 'z') || (p[j] >= 'A' && p[j] <= 'Z')) {
+      tokens[ind][j] = p[j];
+      tokens[ind][j + 1] = '\0';
+    }
+    ind++;
+
+    p = strtok(NULL, " ");
+  }
+
+  int max_frequency = 0;
+  char string_with_mx_freq[100];
+
+  for (int i = 0; i < ind; i++) {
+    int ageei_ki_peyechi = 0;
+    for (int j = 0; j < i; j++) {
+      if (strcmp(tokens[i], tokens[j]) == 0) {
+        ageei_ki_peyechi = 1;
+        break;
+      }
+    }
+    if (ageei_ki_peyechi == 1) { // agei peye gechi, hiseb kora shes
+      continue; //tai
+    }
+    else { // agee hiseb kora hoyni
+      int frequency_of_current_word = 1;
+      for (int j = i + 1; j < ind; j++) {
+        if (strcmp(tokens[i], tokens[j]) == 0) { //pawa geche pore
+          frequency_of_current_word++;
         }
-    }
-    int dp[t + 10] {};
-    dp[0] = 0;
-    for (int i = 1; i <= t; i++) {
-        dp[i] = dp[i - 1];
-        if (pos[i]) {
-            dp[i] = i;
-        }
-    }
-    int mx = 0;
-    for (int i = 1; i <= t; i++) {
-        if (pos[i]) {
-            mx = max(mx, dp[i]);
-            int x = i / 2;
-            int rem = t - x;
-            mx = max(mx, x + dp[rem]);
-        }
-    }
-    cout << mx << endl;
+      }
 
-}
-
-signed main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-
-    ll tcase = 1;
-    // cin>>tcase;
-    for (ll tc = 1; tc <= tcase; tc++) {
-        // cout<<"Case #"<<tc<<": ";
-        solve();
+      //frequency count shes hole current word er frequency beshi hole answer update korte hobe
+      if (max_frequency < frequency_of_current_word) {
+        max_frequency = frequency_of_current_word;
+        strcpy(string_with_mx_freq, tokens[i]);
+      }
     }
+  }
+  printf("String %s has a max frequency of %d\n", string_with_mx_freq, max_frequency);
 
 }
